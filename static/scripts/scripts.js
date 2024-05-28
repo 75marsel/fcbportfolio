65c776aeb1ed1f268157da00ec6f86938700025d
75marsel <jericgappi@gmail.com>
2024-05-28 14:28:30 +0800

    refactor

---
+++ static/scripts/scripts.js
@@ -0,0 +1,12 @@
+// FontAwesome
+const fontAwesomeScript = document.createElement('script');
+fontAwesomeScript.src = "https://kit.fontawesome.com/0b3f1a6994.js";
+fontAwesomeScript.crossOrigin = "anonymous";
+document.head.appendChild(fontAwesomeScript);
+
+// Bootstrap
+const bootstrapScript = document.createElement('script');
+bootstrapScript.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js";
+bootstrapScript.integrity = "sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM";
+bootstrapScript.crossOrigin = "anonymous";
+document.head.appendChild(bootstrapScript);
